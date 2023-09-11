fc95cc921f80963bc96cd2c4698d1a8016bd4458
jga <javivi02@gmail.com>
2023-09-11 14:55:55 +0200

    Pequeño ajuste de formato

--- src/bbdd/connection.js
+++ src/bbdd/connection.js
@@ -2,7 +2,8 @@ import { Sequelize } from 'sequelize'
 import { DB } from '../config.js'
 
 export const sequelize = new Sequelize(
-  DB.database, DB.username, DB.password, {
+  DB.database, DB.username, DB.password,
+  {
   host: DB.host,
   dialect: DB.dialect
 })
